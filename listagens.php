c272ca295789d32f81cc4c76a462c799a4d03878
kauasilval <kaua60033@gmail.com>
2024-11-06 07:46:34 -0300

    a

--- listagens.php
+++ listagens.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
 
-// Funções para buscar dados de cada tabela
+// Funções para buscar dados de cada tabelaa
 function getClientes($pdo) {
     $query = "SELECT * FROM clientes";
     $stmt = $pdo->prepare($query);
